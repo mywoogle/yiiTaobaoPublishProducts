488a3ed8c74ecb5ca9ba5ff0ace78ff780f35faa
mywoogle <917595913@qq.com>
2014-11-30 00:18:58 +0800

    modify the match title

--- protected/tests/publish/MatchTitle.php
+++ protected/tests/publish/MatchTitle.php
@@ -3,7 +3,7 @@ class go2PublishTaobao extends WebTestCase
 {
 	public function testPublish()
 	{
-		$targetResults = Target::model()->findAll('target_title_search=:target_title_search',array(':target_title_search'=>1));
+		$targetResults = Target::model()->findAll('0<target_title_search<=:target_title_search',array(':target_title_search'=>4));
 		foreach($targetResults as $targetResult)
 		{
 			//file_put_contents("publish/reports/test.txt",$targetResult['target_taobao_id'] ."\n", FILE_APPEND);
@@ -26,7 +26,10 @@ class go2PublishTaobao extends WebTestCase
 					xuemianneilicaizhi=:xuemianneilicaizhi and
 					pizhitezhi=:pizhitezhi and
 					xiedicaizhi=:xiedicaizhi and
-					xuekuanpingming=:xuekuanpingming and
+					xuekuanpingming=:xuekuanpingming
+
+				',
+				/*
 					tonggao=:tonggao and
 					xietongkuanshi=:xietongkuanshi and
 					genggao=:genggao and
@@ -36,7 +39,7 @@ class go2PublishTaobao extends WebTestCase
 					zhizhuogongyi=:zhizhuogongyi and
 					tuan=:tuan and
 					shehejijie=:shehejijie
-				',
+				*/
 				array(
 					':xuetongneilicaizhi'=>$targetResult['xuetongneilicaizhi'],
 					':xuetongcaizhi'=>$targetResult['xuetongcaizhi'],
@@ -47,6 +50,7 @@ class go2PublishTaobao extends WebTestCase
 					':pizhitezhi'=>$targetResult['pizhitezhi'],
 					':xiedicaizhi'=>$targetResult['xiedicaizhi'],
 					':xuekuanpingming'=>$targetResult['xuekuanpingming'],
+					/*
 					':tonggao'=>$targetResult['tonggao'],
 					':xietongkuanshi'=>$targetResult['xietongkuanshi'],
 					':genggao'=>$targetResult['genggao'],
@@ -56,12 +60,47 @@ class go2PublishTaobao extends WebTestCase
 					':zhizhuogongyi'=>$targetResult['zhizhuogongyi'],
 					':tuan'=>$targetResult['tuan'],
 					':shehejijie'=>$targetResult['shehejijie'],
+					*/
 				)
 			);
+			
+			$count = count($TaobaoSources);
+			if($count >= 3)
+			{
+					$temResult = Target::model()->find('target_taobao_id=:target_taobao_id',array(':target_taobao_id'=>$targetResult['target_taobao_id']));
+					$temResult->target_title_search = 4;
+					$temResult->target_taobao_title1 = $TaobaoSources[0]['taobao_source_taobao_title'];
+					$temResult->target_taobao_title2 = $TaobaoSources[1]['taobao_source_taobao_title'];
+					$temResult->target_taobao_title3 = $TaobaoSources[2]['taobao_source_taobao_title'];
+					$temResult->source_taobao_id1 = $TaobaoSources[0]['taobao_source_taobao_id'];
+					$temResult->source_taobao_id2 = $TaobaoSources[1]['taobao_source_taobao_id'];
+					$temResult->source_taobao_id3 = $TaobaoSources[2]['taobao_source_taobao_id'];
+					$temResult->source_taobao_keyword1 = $TaobaoSources[0]['key'];
+					$temResult->source_taobao_keyword2 = $TaobaoSources[1]['key'];
+					$temResult->source_taobao_keyword3 = $TaobaoSources[2]['key'];
+					$temResult->save();
+					
+					$TaobaoSource0 = TaobaoSource::model()->find('taobao_source_taobao_id=:taobao_source_taobao_id',array(':taobao_source_taobao_id'=>$TaobaoSources[0]['taobao_source_taobao_id']));
+					$TaobaoSource0->used = 1;
+					$TaobaoSource0->save();
+					$TaobaoSource1 = TaobaoSource::model()->find('taobao_source_taobao_id=:taobao_source_taobao_id',array(':taobao_source_taobao_id'=>$TaobaoSources[1]['taobao_source_taobao_id']));
+					$TaobaoSource1->used = 1;
+					$TaobaoSource1->save();
+					$TaobaoSource2 = TaobaoSource::model()->find('taobao_source_taobao_id=:taobao_source_taobao_id',array(':taobao_source_taobao_id'=>$TaobaoSources[2]['taobao_source_taobao_id']));
+					$TaobaoSource2->used = 1;
+					$TaobaoSource2->save();
+			}else
+			{
+				
+			}
+				
 			foreach($TaobaoSources as $TaobaoSource)
 			{
 				file_put_contents("publish/reports/test.txt",$TaobaoSource['taobao_source_taobao_id'] ."\n", FILE_APPEND);
 				//$this->pause(3000000000);
+				
+
+				
 			}
 			file_put_contents("publish/reports/test.txt","----------------------" ."\n", FILE_APPEND);
 		}
