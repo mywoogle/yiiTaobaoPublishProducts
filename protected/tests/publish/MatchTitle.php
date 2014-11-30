3b49e0261f40f48a940179495ddfe1dd777dfaaa
mywoogle <917595913@qq.com>
2014-11-30 21:59:39 +0800

    MatchTitle all

--- protected/tests/publish/MatchTitle.php
+++ protected/tests/publish/MatchTitle.php
@@ -4,7 +4,9 @@ class go2PublishTaobao extends WebTestCase
 	public function testPublish()
 	{
 		$reportsName = 'publish/reports/MatchTitle/MatchTitle('.date('y-m-d H-i-s',time()).').txt';
-		$targetResults = Target::model()->findAll('0<target_title_search<=:target_title_search',array(':target_title_search'=>4));
+		$targetResults = Target::model()->findAll('0<target_title_search<:target_title_search',array(':target_title_search'=>4));
+		$total = count($targetResults);
+		$totalModify = 0;
 		foreach($targetResults as $targetResult)
 		{
 		
@@ -20,97 +22,97 @@ class go2PublishTaobao extends WebTestCase
 					genggao=:genggao and
 					xiegengkuanshi=:xiegengkuanshi
 				';		
-/*
+			
 			if($targetResult['shangshinianfenjijie'] == '等待修改')
 			{
-				$whereStrint .= ' and shangshinianfenjijie!=:shangshinianfenjijie and ';
+				$whereStrint .= ' and shangshinianfenjijie!=:shangshinianfenjijie';
 				$shangshinianfenjijie = '';
 			}else{
-				$whereStrint .= ' and shangshinianfenjijie=:shangshinianfenjijie and ';
+				$whereStrint .= ' and shangshinianfenjijie=:shangshinianfenjijie';
 				$shangshinianfenjijie = $targetResult['shangshinianfenjijie'];
 			}
 			
 			if($targetResult['fengge'] == '等待修改')
 			{
-				$whereStrint .= 'fengge!=:fengge and ';
+				$whereStrint .= ' and fengge!=:fengge';
 				$fengge = '';
 			}else{
-				$whereStrint .= 'fengge=:fengge and ';
+				$whereStrint .= ' and fengge=:fengge';
 				$fengge = $targetResult['fengge'];
 			}
 			
 			if($targetResult['pizhitezhi'] == '等待修改')
 			{
-				$whereStrint .= 'pizhitezhi!=:pizhitezhi and ';
+				$whereStrint .= ' and pizhitezhi!=:pizhitezhi';
 				$pizhitezhi = '';
 			}else{
-				$whereStrint .= 'pizhitezhi=:pizhitezhi and ';
+				$whereStrint .= ' and pizhitezhi=:pizhitezhi';
 				$pizhitezhi = $targetResult['pizhitezhi'];
 			}
 			
 			if($targetResult['xiedicaizhi'] == '等待修改')
 			{
-				$whereStrint .= 'xiedicaizhi!=:xiedicaizhi and ';
+				$whereStrint .= ' and xiedicaizhi!=:xiedicaizhi';
 				$xiedicaizhi = '';
 			}else{
-				$whereStrint .= 'xiedicaizhi=:xiedicaizhi and ';
+				$whereStrint .= ' and xiedicaizhi=:xiedicaizhi';
 				$xiedicaizhi = $targetResult['xiedicaizhi'];
 			}
 			
 			if($targetResult['xuekuanpingming'] == '等待修改')
 			{
-				$whereStrint .= 'xuekuanpingming!=:xuekuanpingming and ';
+				$whereStrint .= ' and xuekuanpingming!=:xuekuanpingming';
 				$xuekuanpingming = '';
 			}else{
-				$whereStrint .= 'xuekuanpingming=:xuekuanpingming and ';
+				$whereStrint .= ' and xuekuanpingming=:xuekuanpingming';
 				$xuekuanpingming = $targetResult['xuekuanpingming'];
 			}
 
 			if($targetResult['bihefangshi'] == '等待修改')
 			{
-				$whereStrint .= 'bihefangshi!=:bihefangshi and ';
+				$whereStrint .= ' and bihefangshi!=:bihefangshi';
 				$bihefangshi = '';
 			}else{
-				$whereStrint .= 'bihefangshi=:bihefangshi and ';
+				$whereStrint .= ' and bihefangshi=:bihefangshi';
 				$bihefangshi = $targetResult['bihefangshi'];
 			}
 			
 			if($targetResult['liuxingyuansu'] == '等待修改')
 			{
-				$whereStrint .= 'liuxingyuansu!=:liuxingyuansu and ';
+				$whereStrint .= ' and liuxingyuansu!=:liuxingyuansu';
 				$liuxingyuansu = '';
 			}else{
-				$whereStrint .= 'liuxingyuansu=:liuxingyuansu and ';
+				$whereStrint .= ' and liuxingyuansu=:liuxingyuansu';
 				$liuxingyuansu = $targetResult['liuxingyuansu'];
 			}
 			
 			if($targetResult['zhizhuogongyi'] == '等待修改')
 			{
-				$whereStrint .= 'zhizhuogongyi!=:zhizhuogongyi and ';
+				$whereStrint .= ' and zhizhuogongyi!=:zhizhuogongyi';
 				$zhizhuogongyi = '';
 			}else{
-				$whereStrint .= 'zhizhuogongyi=:zhizhuogongyi and ';
+				$whereStrint .= ' and zhizhuogongyi=:zhizhuogongyi';
 				$zhizhuogongyi = $targetResult['zhizhuogongyi'];
 			}
 			
 			if($targetResult['tuan'] == '等待修改')
 			{
-				$whereStrint .= 'tuan!=:tuan and ';
+				$whereStrint .= ' and tuan!=:tuan';
 				$tuan = '';
 			}else{
-				$whereStrint .= 'tuan=:tuan and';
+				$whereStrint .= ' and tuan=:tuan';
 				$tuan = $targetResult['tuan'];
 			}
 			
 			if($targetResult['shehejijie'] == '等待修改')
 			{
-				$whereStrint .= 'shehejijie!=:shehejijie';
+				$whereStrint .= ' and shehejijie!=:shehejijie';
 				$shehejijie = '';
 			}else{
-				$whereStrint .= 'shehejijie=:shehejijie';
+				$whereStrint .= ' and shehejijie=:shehejijie';
 				$shehejijie = $targetResult['shehejijie'];
 			}
-			*/
+			
 			//file_put_contents("$reportsName",$whereStrint."\n", FILE_APPEND);
 			//get all match TaobaoSource
 			$TaobaoSources = TaobaoSource::model()->findAll(
@@ -125,7 +127,6 @@ class go2PublishTaobao extends WebTestCase
 					':xietongkuanshi'=>$targetResult['xietongkuanshi'],
 					':genggao'=>$targetResult['genggao'],
 					':xiegengkuanshi'=>$targetResult['xiegengkuanshi'],
-					/*
 					':shangshinianfenjijie'=>$shangshinianfenjijie,
 					':fengge'=>$fengge,
 					':pizhitezhi'=>$pizhitezhi,
@@ -135,74 +136,50 @@ class go2PublishTaobao extends WebTestCase
 					':liuxingyuansu'=>$liuxingyuansu,
 					':zhizhuogongyi'=>$zhizhuogongyi,
 					':tuan'=>$tuan,
-					':shehejijie'=>$shehejijie,
-					*/
+					':shehejijie'=>$shehejijie
 				)
 			);
 
 			//modify the Target and TaobaoSource
 			$count = count($TaobaoSources);
-			if($count >= 3)
-			{
+			
 				//modify the Target
 				$temResult = Target::model()->find('target_taobao_id=:target_taobao_id',array(':target_taobao_id'=>$targetResult['target_taobao_id']));
-				$temResult->target_title_search = 4;
-				$temResult->target_taobao_title1 = $TaobaoSources[0]->taobao_source_taobao_title;
-				$temResult->target_taobao_title2 = $TaobaoSources[1]->taobao_source_taobao_title;
-				$temResult->target_taobao_title3 = $TaobaoSources[2]->taobao_source_taobao_title;
-				$temResult->source_taobao_id1 = $TaobaoSources[0]->taobao_source_taobao_id;
-				$temResult->source_taobao_id2 = $TaobaoSources[1]->taobao_source_taobao_id;
-				$temResult->source_taobao_id3 = $TaobaoSources[2]->taobao_source_taobao_id;
-				$temResult->source_taobao_keyword1 = $TaobaoSources[0]->key;
-				$temResult->source_taobao_keyword2 = $TaobaoSources[1]->key;
-				$temResult->source_taobao_keyword3 = $TaobaoSources[2]->key;
-				//$temResult->save();
-				file_put_contents("$reportsName",$targetResult['target_taobao_id'] ."---被修改了 3 个标题\n", FILE_APPEND);
-				//modify the TaobaoSource
-				$TaobaoSource0 = TaobaoSource::model()->find('taobao_source_taobao_id=:taobao_source_taobao_id',array(':taobao_source_taobao_id'=>$TaobaoSources[0]->taobao_source_taobao_id));
-				$TaobaoSource0->used = 1;
-				//$TaobaoSource0->save();
-				$TaobaoSource1 = TaobaoSource::model()->find('taobao_source_taobao_id=:taobao_source_taobao_id',array(':taobao_source_taobao_id'=>$TaobaoSources[1]->taobao_source_taobao_id));
-				$TaobaoSource1->used = 1;
-				//$TaobaoSource1->save();
-				$TaobaoSource2 = TaobaoSource::model()->find('taobao_source_taobao_id=:taobao_source_taobao_id',array(':taobao_source_taobao_id'=>$TaobaoSources[2]->taobao_source_taobao_id));
-				$TaobaoSource2->used = 1;
-				//$TaobaoSource2->save();
-			}elseif($count == 2)
-			{
-				//modify the Target
-				$temResult = Target::model()->find('target_taobao_id=:target_taobao_id',array(':target_taobao_id'=>$targetResult['target_taobao_id']));
-				$temResult->target_title_search = 3;
-				$temResult->target_taobao_title1 = $TaobaoSources[0]->taobao_source_taobao_title;
-				$temResult->target_taobao_title2 = $TaobaoSources[1]->taobao_source_taobao_title;
-				$temResult->source_taobao_id1 = $TaobaoSources[0]->taobao_source_taobao_id;
-				$temResult->source_taobao_id2 = $TaobaoSources[1]->taobao_source_taobao_id;
-				$temResult->source_taobao_keyword1 = $TaobaoSources[0]->key;
-				$temResult->source_taobao_keyword2 = $TaobaoSources[1]->key;
-				//$temResult->save();
-				file_put_contents("$reportsName",$targetResult['target_taobao_id'] ."---被修改了 $count 个标题\n", FILE_APPEND);
-				//modify the TaobaoSource
-				$TaobaoSource0 = TaobaoSource::model()->find('taobao_source_taobao_id=:taobao_source_taobao_id',array(':taobao_source_taobao_id'=>$TaobaoSources[0]->taobao_source_taobao_id));
-				$TaobaoSource0->used = 1;
-				//$TaobaoSource0->save();
-				$TaobaoSource1 = TaobaoSource::model()->find('taobao_source_taobao_id=:taobao_source_taobao_id',array(':taobao_source_taobao_id'=>$TaobaoSources[1]->taobao_source_taobao_id));
-				$TaobaoSource1->used = 1;
-				//$TaobaoSource1->save();
-			}elseif($count == 1)
-			{
-				//modify the Target
-				$temResult = Target::model()->find('target_taobao_id=:target_taobao_id',array(':target_taobao_id'=>$targetResult['target_taobao_id']));
-				$temResult->target_title_search = 2;
-				$temResult->target_taobao_title1 = $TaobaoSources[0]->taobao_source_taobao_title;
-				$temResult->source_taobao_id1 = $TaobaoSources[0]->taobao_source_taobao_id;
-				$temResult->source_taobao_keyword1 = $TaobaoSources[0]->key;
-				//$temResult->save();
-				file_put_contents("$reportsName",$targetResult['target_taobao_id'] ."---被修改了 $count 个标题\n", FILE_APPEND);
-				//modify the TaobaoSource
-				$TaobaoSource0 = TaobaoSource::model()->find('taobao_source_taobao_id=:taobao_source_taobao_id',array(':taobao_source_taobao_id'=>$TaobaoSources[0]->taobao_source_taobao_id));
-				$TaobaoSource0->used = 1;
-				//$TaobaoSource0->save();
-			}
+				$flagFrom = intval($temResult->target_title_search);
+				$flagEnd = 0;
+				if($count >= 3)
+				{
+					$flagEnd = 4-$flagFrom;
+				}elseif($count == 2)
+				{
+					$flagEnd = 4-$flagFrom <= 2 ? :2;
+				}elseif($count == 1)
+				{
+					$flagEnd = 4-$flagFrom <= 1 ? :1;
+				}
+				
+				if($count>0)$totalModify++;
+				
+				file_put_contents("$reportsName",$targetResult['target_taobao_id'] ."---正在修改标题", FILE_APPEND);
+				
+				for($i = 0; $i < $flagEnd; $i++) 
+				{
+					//modify the TaobaoSource
+					$temResult->target_title_search = $flagFrom + 1;
+					$flagFrom++;
+					$temResult->target_taobao_title1 = $TaobaoSources[$i]->taobao_source_taobao_title;
+					$temResult->source_taobao_id1 = $TaobaoSources[$i]->taobao_source_taobao_id;
+					$temResult->source_taobao_keyword1 = $TaobaoSources[$i]->key;
+					//$temResult->save();
+					//modify the TaobaoSource
+					$TaobaoSourceTem = TaobaoSource::model()->find('taobao_source_taobao_id=:taobao_source_taobao_id',array(':taobao_source_taobao_id'=>$TaobaoSources[$i]->taobao_source_taobao_id));
+					$TaobaoSourceTem->used = 1;
+					//$TaobaoSourceTem->save();
+					$flag = $flagFrom - 1;
+					file_put_contents("$reportsName",$flag.'、', FILE_APPEND);
+				}
+				file_put_contents("$reportsName","\n", FILE_APPEND);
 		}
+		file_put_contents("$reportsName","------------------修改: $totalModify / 总共: $total ------------------", FILE_APPEND);
 	}
 }
