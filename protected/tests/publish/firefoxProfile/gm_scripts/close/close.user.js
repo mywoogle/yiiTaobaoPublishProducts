4bf01783775a957b2b55f93bc6d899ae318c7fa0
mywoogle <917595913@qq.com>
2014-12-12 22:25:43 +0800

    不显示崩溃恢复页面

---
+++ protected/tests/publish/firefoxProfile/gm_scripts/close/close.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        close
+// @namespace   close
+// @version     1
+// @grant       none
+// ==/UserScript==
+document.getElementById("errorCancel").click()
---
+++ protected/tests/publish/firefoxProfile/publish7/gm_scripts/close/close.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        close
+// @namespace   close
+// @version     1
+// @grant       none
+// ==/UserScript==
+//document.getElementById("errorCancel").click()
+//alert('-----');
